919587dd5a4c259a854803b84b866d64c739754b
CosineAA <rhdwlgns0725@gmail.com>
2023-05-21 18:01:40 +0900

    fix: not solid teleport

--- src/main/kotlin/kr/hqservice/teleport/service/impl/TeleportServiceImpl.kt
+++ src/main/kotlin/kr/hqservice/teleport/service/impl/TeleportServiceImpl.kt
@@ -4,6 +4,7 @@ import kr.hqservice.teleport.extension.async
 import kr.hqservice.teleport.extension.sync
 import kr.hqservice.teleport.repository.TeleportRepository
 import kr.hqservice.teleport.service.TeleportService
+import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.Material
 import org.bukkit.World
@@ -24,17 +25,17 @@ class TeleportServiceImpl(
             for (height in maxHeight downTo 62) {
                 val floorBlock = world.getBlockAt(randomX, height, randomZ)
                 val floorBlockType = floorBlock.type
+
+                if (!floorBlockType.isSolid) continue
+
                 val upBlock = floorBlock.getRelative(BlockFace.UP)
+                val upBlockType = upBlock.type
+
                 val upBlock2 = upBlock.getRelative(BlockFace.UP)
-                if (floorBlockType != Material.AIR && upBlock.type == Material.AIR && upBlock2.type == Material.AIR) {
-                    sync {
-                        val location = upBlock.location.toCenterLocation()
-                        if (floorBlockType.isSolid) {
-                            player.teleport(location)
-                        } else {
-                            player.teleport(location.subtract(0.0, 1.0, 0.0))
-                        }
-                    }
+                val upBlockType2 = upBlock2.type
+
+                if (floorBlockType.isSolid && !upBlockType.isSolid && !upBlockType2.isSolid) {
+                    sync { player.teleport(upBlock.location.toCenterLocation()) }
                     break
                 }
             }
